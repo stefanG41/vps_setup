bedaea68cb06a8b6506afec2c5c25399f9bb4514
stefanG41 <47351072+stefanG41@users.noreply.github.com>
2020-02-07 15:05:58 +0100

    Create vps_setup.sh

---
+++ vps_setup.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+add_autoupdate_1_action () {
+apt-get update && apt-get upgrade -y
+}
+
+install_needed_application_2_action () {
+apt install sudo curl vim fail2ban pwgen -y
+}
+
+modify_application_3_action ()
+{
+cp /etc/fail2ban/jail.conf /etc/fail2ban/jail.local
+IP=$(/sbin/ip -o -4 addr list eth0 | awk '{print $4}' | cut -d/ -f1)
+
+fail2ban-client set sshd addignoreip $IP
+
+
+A="maxretry = 5"
+N="maxretry = 3"
+sed -i "s/$A/$N/g" /etc/fail2ban/jail.local
+
+Ab="bantime  = 10m"
+Nb="bantime  = 3600"
+sed -i "s/$Ab/$Nb/g" /etc/fail2ban/jail.local
+
+fail2ban-client reload sshd
+
+fail2ban-client set sshd addignoreip $IP
+fail2ban-client get sshd ignoreip
+}
+
+add_user_4_action ()
+{
+adduser docker --gecos "First Last,RoomNumber,WorkPhone,HomePhone" --disabled-password
+adduser docker sudo
+echo 'docker:docker$0987' | chpasswd
+
+cp install.sh /home/docker/.
+chown root:docker /home/docker/install.sh
+chmod +x /home/docker/install.sh
+}
+
+
+start_server.sh()
+{
+bash install.sh
+}
+
+
+
+add_autoupdate_1_action
+install_needed_application_2_action
+modify_application_3_action
+add_user_4_action
+echo ''
+echo ''
+echo '############ Please choose next step ##########'
+echo ''
+echo 'Please type your answer now: process or exit? '
+read answer
+echo Answer for the action is: $answer
+
+        if [ "$answer" = "process" ]
+        then
+                start_server.sh
+        else
+                exit
+        fi
