f4472b9efff4d85f313e6ac1002226f8481d3cea
stefanG41 <47351072+stefanG41@users.noreply.github.com>
2020-02-11 16:03:09 +0100

    Update vps_setup.sh

--- vps_setup.sh
+++ vps_setup.sh
@@ -32,9 +32,13 @@ fail2ban-client get sshd ignoreip
 
 add_user_4_action ()
 {
-adduser docker --gecos "First Last,RoomNumber,WorkPhone,HomePhone" --disabled-password
-adduser docker sudo
-echo 'docker:docker$0987' | chpasswd
+echo 'Plaese add user account for Docker, the new account will be added to sudo group. You need to provide three times the new password.'
+read -e docker_user
+echo 'Plaese add password for the new docker user'
+read -e -s docker_password
+
+adduser $docker_user --gecos "First Last,RoomNumber,WorkPhone,HomePhone"
+adduser $docker_user sudo
 }
 
 
